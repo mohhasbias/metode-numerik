8551748b697856877533b2c0d9c83bac4ac675e4
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2014-10-20 14:29:56 +0700

    add hello tree

---
+++ hello_tree.js
@@ -0,0 +1,12 @@
+console.log('hello, tree');
+
+console.log();
+
+console.log('    **');
+console.log('   ****');
+console.log('  ******');
+console.log(' ********');
+console.log('**********');
+console.log('    ||');
+
+console.log();
